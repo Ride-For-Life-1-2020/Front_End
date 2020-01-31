f443f475b5bc5d77c0a38d32fdb9cd17936e8b74
tlewandowski18 <tlewandowski18@gmail.com>
2020-01-30 19:26:22 -0500

    create MotherSignup Form

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import MotherSignup from './MotherSignup'
 
 function App() {
   return (
     <div className="App">
       <h1>Front end</h1>
       <h2>Yup</h2>
+      <MotherSignup />
     </div>
   );
 }
