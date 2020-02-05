f7eded90201bbbdf3d2a02e5aa66e162caea8f36
tlewandowski18 <tlewandowski18@gmail.com>
2020-02-04 23:27:19 -0500

    set validation for delivery info form

--- front-end/src/components/MotherSignup.js
+++ front-end/src/components/MotherSignup.js
@@ -1,5 +1,4 @@
-import React, {useState} from "react"
-import * as yup from 'yup'
+import React, {useState, useEffect} from "react"
 
 import { 
     Container, 
@@ -15,8 +14,9 @@ import {
 } from './styled-components'
 
 const MotherSignup = props => {
-    console.log(props);
+    
     const data = props.location.state.user
+
     const [deliveryInfo, setDeliveryInfo] = useState({...data,
         dueDate: '',
         hospitalName: '',
@@ -31,8 +31,7 @@ const MotherSignup = props => {
         hospitalNameError: '',
         hospitalAddressError: '',
         hospitalCityError: '',
-        hospitalPhoneError: '',
-        termsAndConditionsError: false 
+        hospitalPhoneError: ''
     })
     
     
@@ -41,51 +40,90 @@ const MotherSignup = props => {
         setDeliveryInfo({...deliveryInfo, [e.target.name]: e.target.value})
     }
 
-    const handleSubmit = e => {
-        const err = validate();
-        if (err) {
-            e.preventDefault()
-        }
-    }
 
     const validate = () => {
-        
         let isErr = false
 
-        if (deliveryInfo.dueDate.length < 1) {
-            setDeliveryInfoErrors.dueDateError("Due Date is a required filed")
-            isErr = true
+        const errors = {
+            dueDateError: '',
+            hospitalNameError: '',
+            hospitalAddressError: '',
+            hospitalCityError: '',
+            hospitalPhoneError: ''
         }
+
+        if (deliveryInfo.dueDate.length < 1) {
+            isErr = true
+            errors.dueDateError = "Due Date is a required field";
+        }
+
+        if (deliveryInfo.hospitalName.length < 1) {
+            isErr = true
+            errors.hospitalNameError= "Hospital Name is a required field";
+        }
+
+        if (deliveryInfo.hospitalAddress.length < 1) {
+            isErr = true
+            errors.hospitalAddressError = "Hospital Address is a required field";
+        }
+
+        if (deliveryInfo.hospitalCity.length < 1) {
+            isErr = true
+            errors.hospitalCityError = "Hospital City is a required field";
+        }
+
+        if (deliveryInfo.hospitalPhone.length < 10) {
+            isErr = true
+            errors.hospitalPhoneError = "Please enter a phone number in this format: (+256) 772-000-0000";
+        }
+
+
+        if (isErr) {
+            setDeliveryInfoErrors({...deliveryInfoErrors, ...errors})
+        }
+
+        return isErr
     }
 
-    console.log(deliveryInfo)
-
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const err = validate()
+        if (!err) {
+            console.log(deliveryInfo)
+        }
+    }
+    
     
     return (
         <Container>
             <ClearFix px="15px" />
-            <FormWrapper>
+            <FormWrapper onSubmit={handleSubmit}>
                 <H2>Tell us about your delivery</H2>
                 <TextFieldWrapper>
                     <Label htmlFor="dueDate">Due date</Label>
                     <TextField id="dueDate" value={deliveryInfo.dueDate} type="date" name="dueDate" onChange={handleChange} />
                 </TextFieldWrapper>
+                {deliveryInfoErrors.dueDateError.length > 1 ? <Error>{deliveryInfoErrors.dueDateError}</Error> : null}
                 <TextFieldWrapper>
                     <Label htmlFor="hospitalName">Hospital Name</Label>
                     <TextField placeholder="Hospital Name" id="hospitalName" value={deliveryInfo.hospitalName} type="text" name="hospitalName" onChange={handleChange} />
                 </TextFieldWrapper>
+                {deliveryInfoErrors.hospitalNameError.length > 1 ? <Error>{deliveryInfoErrors.hospitalNameError}</Error> : null}
                 <TextFieldWrapper>
                     <Label htmlFor="hospitalAddress">Hospital Address</Label>
                     <TextField placeholder="Address" id="hospitalAddress" value={deliveryInfo.hospitalAddress} type="text" name="hospitalAddress" onChange={handleChange} />
                 </TextFieldWrapper>
+                {deliveryInfoErrors.hospitalAddressError.length > 1 ? <Error>{deliveryInfoErrors.hospitalAddressError}</Error> : null}
                 <TextFieldWrapper>
                     <Label htmlFor="hospitalCity">Hospital City</Label>
                     <TextField placeholder="Ex. Kampala" id="hospitalCity" value={deliveryInfo.hospitalCity} type="text" name="hospitalCity" onChange={handleChange} />
                 </TextFieldWrapper>
+                {deliveryInfoErrors.hospitalCityError.length > 1 ? <Error>{deliveryInfoErrors.hospitalCityError}</Error> : null}
                 <TextFieldWrapper>
                     <Label htmlFor="hospitalPhone">Hospital Phone</Label>
                     <TextField placeholder="+256 772-000-000" id="hospitalPhone" value={deliveryInfo.hospitalPhone} type="phone" name="hospitalPhone" onChange={handleChange} />
                 </TextFieldWrapper>
+                {deliveryInfoErrors.hospitalPhoneError.length > 1 ? <Error>{deliveryInfoErrors.hospitalPhoneError}</Error> : null}
                 <ClearFix px="10px" />
                 <Checkbox handleChange={handleChange} value={deliveryInfo.termsAndConditions}>
                     <a href="#">
@@ -93,7 +131,7 @@ const MotherSignup = props => {
                     </a>
                 </Checkbox>
                 <ClearFix px="15px" />
-                <Button type="submit">Submit</Button>
+                <Button type="submit" >Submit</Button>
             </FormWrapper>
         </Container>
     )
