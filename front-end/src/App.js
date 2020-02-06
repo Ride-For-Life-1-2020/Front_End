c5050ec33569c3a8a9c676e9d4b382fd072f634c
Daniel Kanangila <daniel.kanangila@gmail.com>
2020-02-05 20:08:54 -0500

    fixed form submit handler

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -20,38 +20,6 @@ function App() {
   const state = useSelector(state => state);
   const isLogged = useSelector(state => state.root.isLoggedIn);
 
-  // This state can contain the mother's or dirver's signup information. 
-  // The data come from signup forms and can be present like this
-  /**
-    object {
-      userType: string 'mother' || 'driver'
-
-      // General information (same for the mother and driver)
-      // Signup Process (step1)
-      firstName: string
-      lastName: stirng
-      eamil: string
-      phone: string
-      address: string
-      city: string
-      userName: string
-      password:  string
-
-      // Specific information
-      // Signup Process (step2. step3)
-      ===========================================================================================
-      = Mother (sptep2)                     "  Driver (step2)           " Driver (step3)        =
-      =============================================================== ===========================
-      = dueDate: string (date: mm/dd/yyyy)  " dateOgBirth: string       " vehicleMake: string   =
-      = hospitalName: string                " licenseNumber: string     " vehicleModel: string  =         
-      = hospitalAddress: string             " insuranceCompany: string  " year: string          =
-      = hospitalCity: string                " policyNumber: string      " licensePlate: string  =
-      = hospitalPhone: string               "                           " vin: string           =
-      =========================================================================================
-    }
-   */ 
-   const [newUserInfo, setNewUserInfo] = useState({})
-
   // useEffect( () => {
   //   axios.get('/login')
   //   .then( response => {
@@ -69,11 +37,6 @@ function App() {
 
   const getUserType = () => isMother ? 'mother' : 'driver';
 
-  const handleUserFormSubmit = data => {
-    setNewUserInfo(data.data);
-
-    /** ===== POST REQUEST HERE ======= */
-  }
   console.log(state);
   return (
 
@@ -81,15 +44,15 @@ function App() {
       <AppBar isLogged={isLogged} />
       <Switch>
         <Route exact path="/" render={props => <HomePage history={props.history} />} />
-        <Route exact path="/signup" render={props => <UserSignup history={props.history} handleUserFormSubmit={handleUserFormSubmit}  userType={getUserType()} />} />
+        <Route exact path="/signup" render={props => <UserSignup history={props.history}  userType={getUserType()} />} />
         <Route path="/signup/whoami" render={props => <WhoAmI history={props.history} onUserType={onUserType} />} />
-        <Route path="/signup/mother" render={props => <MotherSignup {...props} handleUserFormSubmit={handleUserFormSubmit} />} />
+        <Route path="/signup/mother" render={props => <MotherSignup {...props} />} />
         <Route exact path="/sign-in" component={LoginForm} />
-        <Route exact path="/signup" render={props => <UserSignup history={props.history} handleUserFormSubmit={handleUserFormSubmit}  userType={getUserType()} />} />
+        <Route exact path="/signup" render={props => <UserSignup history={props.history}  userType={getUserType()} />} />
         <Route path="/signup/whoami" render={props => <WhoAmI history={props.history} onUserType={onUserType} />} />
-        <Route path="/signup/mother" render={props => <MotherSignup handleUserFormSubmit={handleUserFormSubmit} />} />
-        <Route exact path="/signup/driver/step/1" render={props => <DriverInfoForm history={props.history} handleUserFormSubmit={handleUserFormSubmit} />} />
-        <Route path="/signup/driver/step/2" render={props => <VehicleInfo handleUserFormSubmit={handleUserFormSubmit} />} />
+        <Route path="/signup/mother" render={props => <MotherSignup />} />
+        <Route exact path="/signup/driver/step/1" render={props => <DriverInfoForm {...props} />} />
+        <Route path="/signup/driver/step/2" render={props => <VehicleInfo />} />
        </Switch>
       <Footer />
     </div>
