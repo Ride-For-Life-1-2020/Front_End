0209c20003f22b606459a672de9a1e1d0976aeaf
Daniel Kanangila <daniel.kanangila@gmail.com>
2020-02-05 20:10:02 -0500

    updated

---
+++ front-end/src/components/DriverDashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Container } from './styled-components'
+
+
+const DriverDashboard = () => {
+
+    return(
+        <Container>
+            
+        </Container>
+    )
+}
+
+export default DriverDashboard;
